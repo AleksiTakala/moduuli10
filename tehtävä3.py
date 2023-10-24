27fc8d17e4b9e24877cd81192aa68e647c677330
at345 <at.345688@gmail.com>
2023-10-24 23:38:45 +0300

    3

---
+++ tehtävä3.py
@@ -0,0 +1,55 @@
+class Hissi:
+    def __init__(self, alin, ylin):
+        self.alin = alin
+        self.ylin = ylin
+        self.kerros = alin
+
+    def siirry(self, Siirry):
+        if Siirry > self.kerros:
+            siirrä = Siirry - self.kerros
+            for i in range(siirrä):
+                if self.kerros == self.ylin:
+                    return
+                else:
+                    self.kerros_ylös()
+        elif self.kerros > Siirry:
+            siirrä = self.kerros - Siirry
+            for i in range(siirrä):
+                if self.kerros == self.alin:
+                    return
+                else:
+                    self.kerros_alas()
+
+    def kerros_alas(self):
+        self.kerros -= 1
+        print(f"kerroksesi on {self.kerros}")
+        return
+    def kerros_ylös(self):
+        self.kerros += 1
+        print(f"kerroksesi on {self.kerros}")
+        return
+class Talo:
+    def __init__(self, alinkerros, ylinkerros, hissit):
+        self.hissilkm = []
+        self.alinkerros = alinkerros
+        self.ylinkerros = ylinkerros
+
+        for i in range(hissit):
+            määrä = Hissi(self.alinkerros, self.ylinkerros)
+            määrä.num = i + 1
+            self.hissilkm.append(määrä)
+    def aja_hissiä(self, hissinum, kerros):
+        nykyhissi = self.hissilkm[hissinum-1]
+        print(f"Olet hississä {nykyhissi.num}")
+        nykyhissi.siirry(kerros)
+    def palohälytys(self):
+        nykyhissi = self.hissilkm[2]
+        print(f"Palohälytys kaikki hissit pohjakerrokseen")
+        for i in range(len(self.hissilkm)):
+            nykyhissi.siirry(self.alinkerros)
+            print(f"Hissi {i+1} on kerroksessa {self.alinkerros}")
+
+
+H = Talo(0, 5 ,4)
+H.aja_hissiä(4, 4)
+H.palohälytys()
